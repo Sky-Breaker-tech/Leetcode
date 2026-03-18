ada4a07c54f3bee864169266cb5877bbad8c82ca
lohithakshay gudi <lohithakshaygudi@gmail.com>
2026-03-18 09:59:20 +0530

    Time: 114 ms (27.19%) | Memory: 57.6 MB (77.15%) - LeetSync

---
+++ 1512-design-underground-system/design-underground-system.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class UndergroundSystem {
+
+    class Pair {
+        String station;
+        int time;
+
+        Pair(String station, int time) {
+            this.station = station;
+            this.time = time;
+        }
+    }
+
+    // id → check-in info
+    Map<Integer, Pair> checkInMap;
+
+    // route → {totalTime, count}
+    Map<String, int[]> routeMap;
+
+    public UndergroundSystem() {
+        checkInMap = new HashMap<>();
+        routeMap = new HashMap<>();
+    }
+
+    public void checkIn(int id, String stationName, int t) {
+        checkInMap.put(id, new Pair(stationName, t));
+    }
+
+    public void checkOut(int id, String stationName, int t) {
+        Pair p = checkInMap.get(id);
+
+        String route = p.station + "-" + stationName;
+        int travelTime = t - p.time;
+
+        int[] data = routeMap.getOrDefault(route, new int[2]);
+        data[0] += travelTime; // total time
+        data[1] += 1;          // count
+
+        routeMap.put(route, data);
+
+        checkInMap.remove(id);
+    }
+
+    public double getAverageTime(String startStation, String endStation) {
+        String route = startStation + "-" + endStation;
+        int[] data = routeMap.get(route);
+
+        return (double) data[0] / data[1];
+    }
+}
